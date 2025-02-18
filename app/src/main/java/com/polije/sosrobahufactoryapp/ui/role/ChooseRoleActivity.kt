d829e4312b17033a5b533aba86d1abeb9b6d4770
SiTelur <gantengrizky440@gmail.com>
2025-02-18 08:22:12 +0700

    Adding Git Ignore

---
+++ app/src/main/java/com/polije/sosrobahufactoryapp/ui/role/ChooseRoleActivity.kt
@@ -0,0 +1,23 @@
+package com.polije.sosrobahufactoryapp.ui.role
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.polije.sosrobahufactoryapp.databinding.ActivityRoleBinding
+import com.polije.sosrobahufactoryapp.ui.factory.login.LoginActivity
+
+class ChooseRoleActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRoleBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRoleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.cvPabrik.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
