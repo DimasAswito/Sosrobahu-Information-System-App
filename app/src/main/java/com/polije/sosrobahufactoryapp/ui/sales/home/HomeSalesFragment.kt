2bee41a7579fdb99a5831e9b1a8a77c3c2e324d4
DimasAswito <dimasaswito@gmail.com>
2025-05-11 09:53:50 +0700

    change home layout all role and test loading home sales

--- app/src/main/java/com/polije/sosrobahufactoryapp/ui/sales/home/HomeSalesFragment.kt
+++ app/src/main/java/com/polije/sosrobahufactoryapp/ui/sales/home/HomeSalesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewStub
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
@@ -12,6 +13,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.findNavController
 import com.polije.sosrobahufactoryapp.R
 import com.polije.sosrobahufactoryapp.databinding.FragmentHomeSalesBinding
+import com.polije.sosrobahufactoryapp.databinding.LoadingOverlayBinding
 import com.polije.sosrobahufactoryapp.ui.agen.dashboard.DashboardAgenFragmentDirections
 import com.polije.sosrobahufactoryapp.utils.toRupiah
 import kotlinx.coroutines.flow.collectLatest
@@ -23,68 +25,63 @@ class HomeSalesFragment : Fragment() {
     private var _binding: FragmentHomeSalesBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var loadingBinding: LoadingOverlayBinding
+
     private val viewModel: HomeSalesViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeSalesBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentHomeSalesBinding.inflate(inflater, container, false)
+        loadingBinding = LoadingOverlayBinding.inflate(inflater)
+        (binding.root as ViewGroup).addView(loadingBinding.root)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewLifecycleOwner.lifecycleScope.launch {
 
+        viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.isLogged.collectLatest {
                     if (!it) {
-                        Toast.makeText(requireContext(), "Logout berhasil", Toast.LENGTH_SHORT)
-                            .show()
+                        Toast.makeText(requireContext(), "Logout berhasil", Toast.LENGTH_SHORT).show()
                         val action =
                             DashboardAgenFragmentDirections.actionDashboardAgenFragmentToAgenLoginFragment()
-                        activity?.findNavController(R.id.fragmentContainerView)
-                            ?.navigate(action)
+                        activity?.findNavController(R.id.fragmentContainerView)?.navigate(action)
                     }
                 }
             }
-
-
         }
 
         lifecycleScope.launch {
             viewModel.state.collectLatest { state ->
                 when (state) {
                     is HomeSalesState.Failure -> {
-                        Toast.makeText(requireContext(), state.errorMessage, Toast.LENGTH_LONG)
-                            .show()
+                        loadingBinding.loadingLayout.visibility = View.GONE
+                        Toast.makeText(requireContext(), state.errorMessage, Toast.LENGTH_LONG).show()
                     }
 
                     HomeSalesState.Initial -> {
-                        binding.progressBar2.visibility = View.GONE
+                        loadingBinding.loadingLayout.visibility = View.GONE
                     }
 
-
                     HomeSalesState.Loading -> {
-                        binding.progressBar2.visibility = View.VISIBLE
+                        loadingBinding.loadingLayout.visibility = View.VISIBLE
                     }
 
                     is HomeSalesState.Success -> {
+                        loadingBinding.loadingLayout.visibility = View.GONE
 
-//                        val namaSales = state.dashboardResponse.namaSales.split(" ").firstOrNull() ?: ""
-//                        binding.headerTextSales.text = "Selamat datang $namaSales,"
+                        val namaSales = state.dashboardResponse.namaSales.split(" ").firstOrNull() ?: ""
+                        binding.headerTextSales.text = "Selamat datang $namaSales,"
 
-//                        binding.totalStokSales.text = state.dashboardResponse.totalStokKeseluruhan.toString()
-//                        binding.jumlahToko.text   = state.dashboardResponse.jumlahToko.toString()
-//                        binding.modalSales.text =
-//                            state.dashboardResponse.totalPendapatan?.toRupiah()
-//
-//                        binding.imgTopProduct. // tak binding sendiri ngko lur
-//                        binding.topProductNameSales.text = state.dashboardResponse.topProduct
-
+                        binding.totalStokSales.text = state.dashboardResponse.totalStok.toString()
+                        binding.jumlahToko.text = state.dashboardResponse.jumlahToko.toString()
+                        binding.modalSales.text = state.dashboardResponse.totalPrice?.toRupiah()
                     }
-
                 }
             }
         }
