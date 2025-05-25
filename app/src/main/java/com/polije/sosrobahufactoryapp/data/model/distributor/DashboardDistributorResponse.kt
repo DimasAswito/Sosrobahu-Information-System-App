7007650c38d01467742d3933cee4bcb76139bb01
SiTelur <gantengrizky440@gmail.com>
2025-05-25 19:46:30 +0700

    Update DashboardDistributorResponse.kt

--- app/src/main/java/com/polije/sosrobahufactoryapp/data/model/distributor/DashboardDistributorResponse.kt
+++ app/src/main/java/com/polije/sosrobahufactoryapp/data/model/distributor/DashboardDistributorResponse.kt
@@ -22,7 +22,7 @@ data class DashboardDistributorResponse(
      * Key is the month string (e.g. "2025-03")
      */
     @SerialName("pesananPerBulan")
-    val pesananPerBulan: List<Map<String, PesananDistributorPerBulan>> = emptyList(),
+    val pesananPerBulan: Map<String, PesananDistributorPerBulan> = emptyMap(),
 
     @SerialName("availableYears")
     val availableYears: List<Int> = emptyList(),
